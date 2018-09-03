849a40e1be6caf19f6c3d13b2f03e1146604f1e0
baris_000 <barisozcetin@gmail.com>
2018-09-02 17:46:11 -0700

    svg icons

---
+++ client/src/components/icons/CardUpdate.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const CardUpdate = () => {
+	return (
+		<svg
+			id="i-edit"
+			viewBox="0 0 32 32"
+			width="20"
+			height="20"
+			fill="orange"
+			stroke="currentcolor"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth="2"
+		>
+			<path d="M30 7 L25 2 5 22 3 29 10 27 Z M21 6 L26 11 Z M5 22 L10 27 Z" />
+		</svg>
+	);
+};
+
+export default CardUpdate;
---
+++ client/src/components/icons/Plus.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Plus = () => {
+	return (
+		<svg
+			id="i-plus"
+			viewBox="0 0 32 32"
+			width="32"
+			height="32"
+			fill="none"
+			stroke="white"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth="2"
+		>
+			<path d="M16 2 L16 30 M2 16 L30 16" />
+		</svg>
+	);
+};
+
+export default Plus;
